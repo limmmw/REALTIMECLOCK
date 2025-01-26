69029b9a8c65fa1a5868646fb557ec3d4e066fa9
limmmw <nurhhalim4@gmail.com>
2025-01-26 16:02:20 +0700

    rtc arduino program

---
+++ RTC_CODE/RTC_CODE.ino
@@ -0,0 +1,32 @@
+#include "RTClib.h"
+RTC_DS3231 rtc;
+char daysOfTheWeek[7][12] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"};
+int jam, menit, detik;
+int tanggal, bulan, tahun;
+String hari;
+
+void setup () {
+  Serial.begin(9600);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+}
+
+void loop () {
+  DateTime now = rtc.now();
+  jam     = now.hour();
+  menit   = now.minute();
+  detik   = now.second();
+  tanggal = now.day();
+  bulan   = now.month();
+  tahun   = now.year();
+  hari    = daysOfTheWeek[now.dayOfTheWeek()];
+  Serial.println(String() + hari + ", " + tanggal + "-" + bulan + "-" + tahun);
+  Serial.println(String() + jam + ":" + menit + ":" + detik);
+  Serial.println();
+  delay(1000);
+}
